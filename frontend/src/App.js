49bdaf1e1f14b8a5216f8353d92ccc49fd73deda
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-03-04 17:18:25 +0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import AdminDashboard from './components/AdminDashboard';
-import ManagerDashboard from './components/ManagerDashboard';
-import Logout from './components/Logout';
 
-
-const App = () => {
-  const [role, setRole] = useState(null);
-
+import React from 'react';
+import Lo from './lo';
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={!role ? <Login setRole={setRole} /> : role === 'admin' ? <AdminDashboard /> : <ManagerDashboard />} />
-          <Route path="/logout" element={<Logout setRole={setRole} />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      hello
+<Lo />
+    </div>
   );
-};
+}
 
 export default App;
